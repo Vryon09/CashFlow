074977d6b80684a6198c7165959fab4932e72b6f
Vryon Antonio <vryonantonio@gmail.com>
2024-12-15 08:58:07 +0800

    search result independent scroll bar

--- src/components/Popup/Search/SearchResults.jsx
+++ src/components/Popup/Search/SearchResults.jsx
@@ -3,6 +3,12 @@ import Modal from "../Modal/Modal";
 import SearchResult from "./SearchResult";
 import styles from "./SearchResults.module.css";
 
+const style = {
+  padding: "1em",
+  width: "70%",
+  height: "90dvh",
+};
+
 function SearchResults({ list }) {
   const { initialQuantity, dispatch } = useItems();
 
@@ -12,40 +18,44 @@ function SearchResults({ list }) {
   }
 
   return (
-    <Modal closing="closeSearchModal" style={styles.searchResults}>
-      <div className={styles.header}>
-        <h2>Search Results:</h2>
+    <Modal styleObject={style}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h2>Search Results:</h2>
 
-        <div className={styles.quantity}>
-          <button className={styles.btn} onClick={handleDec}>
-            -
-          </button>
-          <p>{initialQuantity}</p>
-          <button
-            className={styles.btn}
-            onClick={() =>
-              dispatch({ type: "changeInitialQuantity", payload: 1 })
-            }
-          >
-            +
-          </button>
+          <div className={styles.quantity}>
+            <button className={styles.btn} onClick={handleDec}>
+              -
+            </button>
+            <p>{initialQuantity}</p>
+            <button
+              className={styles.btn}
+              onClick={() =>
+                dispatch({ type: "changeInitialQuantity", payload: 1 })
+              }
+            >
+              +
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.searchResults}>
+          {list.length === 0 ? (
+            <p className={styles.noItems}>No items found.</p>
+          ) : (
+            <ul className={styles.results}>
+              {list.map((item, i) => (
+                <SearchResult
+                  name={item.productName}
+                  code={item.code}
+                  price={item.price}
+                  key={i}
+                />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
-
-      {list.length === 0 ? (
-        <p className={styles.noItems}>No items found.</p>
-      ) : (
-        <ul className={styles.results}>
-          {list.map((item, i) => (
-            <SearchResult
-              name={item.productName}
-              code={item.code}
-              price={item.price}
-              key={i}
-            />
-          ))}
-        </ul>
-      )}
     </Modal>
   );
 }
